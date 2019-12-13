4f8a6962cf16f5d910aad88bef1b8fa0b5ccdc0d
System Administrator <root@c02mg204fh04.windows.ebi.ac.uk>
2019-12-13 11:46:09 +0000

    Updates

---
+++ precache-manifest.7714bd63b83843e36cfc1b393925d691.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea74bd3d1d6c15d8b20922743aed860",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e3a77270f699378346c",
+    "url": "/static/css/main.94898b66.chunk.css"
+  },
+  {
+    "revision": "d5bb6098783985485ba2",
+    "url": "/static/js/2.c2850d35.chunk.js"
+  },
+  {
+    "revision": "8e3a77270f699378346c",
+    "url": "/static/js/main.933951c7.chunk.js"
+  },
+  {
+    "revision": "88ffb840c6bb114877d7",
+    "url": "/static/js/runtime-main.ab0f3868.js"
+  }
+]);
